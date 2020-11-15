1c8804b5ccc1e7df62f2d1600ec3d01bdfc4622c
Alan Hanson <alan@A-CM-MBP.local>
2020-11-14 22:32:34 -0800

    New function instance_to_struct.
    Moved the inner loop to a new function that will do the work of
    copying the fields of the EC2 Instance struct over to our new
    smaller Instance struct that has just the fields we need.
    
    Renamed name_len to name_max

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,76 @@ pub struct Instance {
     spot: String,
 }
 
-async fn find_instances(region: Region, all_instances: &mut Vec<Instance>) -> usize {
+/// Take a valid EC2 instance from rusoto and use the values
+/// in it to fill up a new Instance struct.  Once it's filled,
+/// add the new Instance to the provided vector of all instances.
+///
+/// Return the length of the name we found.
+fn instance_to_struct(x: &rusoto_ec2::Instance, all_instances: &mut Vec<Instance>) -> usize {
+    let mut name_max = 0;
+    let mut inst_name: String = "".to_string();
+    for (_, y) in x.tags.iter().enumerate() {
+        for (_, z) in y.iter().enumerate() {
+            let n = match &z.key {
+                Some(p) => p,
+                None => continue,
+            };
+            if n == "Name" {
+                inst_name = z.value.clone().unwrap_or_default();
+                name_max = inst_name.len();
+            }
+        }
+    }
+    let mut inst_az: String = "".to_string();
+    if x.placement.is_some() {
+        inst_az = x
+            .placement
+            .as_ref()
+            .unwrap()
+            .availability_zone
+            .clone()
+            .unwrap_or_default();
+    }
 
+    let mut spot = " ".to_string();
+    if x.instance_lifecycle.is_some() && x.instance_lifecycle.clone().unwrap() == "spot" {
+        spot = "S".to_string();
+    }
+    let mut inst_state: String = "?".to_string();
+    let mut inst_state_letter: String = "?".to_string();
+    if let Some(state) = &x.state {
+        inst_state = state.name.clone().unwrap_or_default();
+        if let Some(code) = &state.code {
+            inst_state_letter = match code {
+                0 => 'P'.to_string(),
+                16 => 'R'.to_string(),
+                32 => 'D'.to_string(),
+                48 => 'T'.to_string(),
+                64 => 's'.to_string(),
+                80 => 'S'.to_string(),
+                _ => '?'.to_string(),
+            }
+        }
+    }
+    let ai: Instance = Instance {
+        name: inst_name,
+        instance_id: x.instance_id.clone().unwrap_or_default(),
+        az: inst_az,
+        pub_ip: x.public_ip_address.clone().unwrap_or_default(),
+        inst_type: x.instance_type.clone().unwrap_or_default(),
+        pri_ip: x.private_ip_address.clone().unwrap_or_default(),
+        subnet: x.subnet_id.clone().unwrap_or_default(),
+        key_name: x.key_name.clone().unwrap_or_default(),
+        state: inst_state,
+        launch_time: x.launch_time.clone().unwrap_or_default(),
+        vpc: x.vpc_id.clone().unwrap_or_default(),
+        isl: inst_state_letter,
+        spot,
+    };
+    all_instances.push(ai);
+    name_max
+}
+async fn find_instances(region: Region, all_instances: &mut Vec<Instance>) -> usize {
     let ec2_client = Ec2Client::new(region);
     let describe_instances_request = DescribeInstancesRequest::default();
 
@@ -38,7 +106,7 @@ async fn find_instances(region: Region, all_instances: &mut Vec<Instance>) -> us
         }
     };
 
-    let mut name_len = 0;
+    let mut name_max = 0;
     // println!("{:#?}", all.reservations);
     if let Some(reservations) = all.reservations {
         for res in reservations {
@@ -48,71 +116,17 @@ async fn find_instances(region: Region, all_instances: &mut Vec<Instance>) -> us
             };
 
             // Each instance is a vector.  And for me there is never
-            // more than one item in that vector.
+            // more than one item in that vector.  Not sure if that is
+            // always true, or just true in the way I use AWS.
             for (_, x) in inst.iter().enumerate() {
-                let mut inst_name: String = "".to_string();
-                for (_, y) in x.tags.iter().enumerate() {
-                    for (_, z) in y.iter().enumerate() {
-                        let n = match &z.key {
-                            Some(p) => p,
-                            None => continue
-                        };
-                        if n == "Name" {
-                            inst_name = z.value.clone().unwrap_or_default();
-                            let len = inst_name.len();
-                            if len > name_len {
-                                name_len = len;
-                            }
-                        }
-                    }
+                let max = instance_to_struct(x, all_instances);
+                if max > name_max {
+                    name_max = max
                 }
-                let mut inst_az: String = "".to_string();
-                if  x.placement.is_some() {
-                    inst_az = x.placement.as_ref().unwrap().availability_zone.clone().unwrap_or_default();
-                }
-
-                let mut spot = " ".to_string();
-                if x.instance_lifecycle.is_some() &&
-                   x.instance_lifecycle.clone().unwrap() == "spot" {
-                    println!("spotttt");
-                    spot = "S".to_string();
-                }
-                let mut inst_state: String = "?".to_string();
-                let mut inst_state_letter: String = "X".to_string();
-                if let Some(state) = &x.state {
-                    inst_state = state.name.clone().unwrap_or_default();
-                    if let Some(code) = &state.code {
-                        inst_state_letter = match code {
-                            0 => 'P'.to_string(),
-                            16 => 'R'.to_string(),
-                            32 => 'D'.to_string(),
-                            48 => 'T'.to_string(),
-                            64 => 's'.to_string(),
-                            80 => 'S'.to_string(),
-                            _ => '?'.to_string(),
-                        }
-                    }
-                }
-                let ai: Instance = Instance {
-                    name: inst_name,
-                    instance_id: x.instance_id.clone().unwrap_or_default(),
-                    az: inst_az,
-                    pub_ip: x.public_ip_address.clone().unwrap_or_default(),
-                    inst_type: x.instance_type.clone().unwrap_or_default(),
-                    pri_ip: x.private_ip_address.clone().unwrap_or_default(),
-                    subnet: x.subnet_id.clone().unwrap_or_default(),
-                    key_name: x.key_name.clone().unwrap_or_default(),
-                    state: inst_state,
-                    launch_time: x.launch_time.clone().unwrap_or_default(),
-                    vpc: x.vpc_id.clone().unwrap_or_default(),
-                    isl: inst_state_letter,
-                    spot: spot,
-                };
-                all_instances.push(ai);
             }
         }
     }
-    return name_len
+    name_max
 }
 
 #[tokio::main]
@@ -122,11 +136,13 @@ async fn main() {
     let mut max = 0;
     // This will be redone as a loop, I promise!
     /*
-    let name_len: usize = find_instances(Region::UsEast1, &mut all_instances).await;
+    let name_max: usize = find_instances(Region::UsEast1, &mut all_instances).await;
     if name_len > max { max = name_len };
     */
     let name_len: usize = find_instances(Region::UsEast2, &mut all_instances).await;
-    if name_len > max { max = name_len };
+    if name_len > max {
+        max = name_len
+    };
     /*
     let name_len: usize = find_instances(Region::UsWest1, &mut all_instances).await;
     if name_len > max { max = name_len };
